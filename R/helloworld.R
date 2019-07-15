399916f5e875a9027ac00a823ebc9d7bff038c16
Laurette Mhlanga <laurette@debmower_ajd>
2019-07-15 14:52:30 +0200

    First Commit

---
+++ R/helloworld.R
@@ -0,0 +1,14 @@
+
+#' helloworld
+#'
+#' @param
+#' @param
+#' @return prints \code{"hello world"}
+#' @examples
+#' helloworld()
+#'
+#' @export
+
+helloworld <- function(){
+  print("hello world")
+}
